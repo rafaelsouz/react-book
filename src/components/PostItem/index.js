6dde5d2e12c93cb93f0d98c7150d37428dca2bb0
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-19 12:54:54 -0300

    Post item feito

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -9,7 +9,6 @@ function PostHeader() {
         <span>Júlio Alcantara</span>
         <span>04 Jun 2019</span>
       </div>
-      <p>Pessoal, alguém sabe se a Rocketseat está contratando?</p>
     </div>
   );
 }
@@ -17,16 +16,30 @@ function PostHeader() {
 function PostComments() {
   return (
     <div className='postComment'>
-
+      <div className="divider" />
+        <div className="comment">
+          FOTO
+          <p>
+            <span>Diego Fernandes</span>
+            A Rocketseat está sempre em busca de novos membros para o time, e geralmente 
+            ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de 
+            devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar 
+            gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem 
+            mesmo, esse comentário é real)
+          </p>
+        </div>
     </div>
   );
 }
 
 function PostItem() {
+  return (
   <div className='postItem'>
     <PostHeader />
+    <p>Pessoal, alguém sabe se a Rocketseat está contratando?</p>
     <PostComments />
   </div>
+  )
 }
 
 
