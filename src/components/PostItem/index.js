5d10a1bc743bb85fe73ba3fd0e83420e3f68cd5d
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-19 21:00:18 -0300

    Estilizando os componentes

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -4,8 +4,8 @@ import './styles.css';
 function PostHeader() {
   return (
     <div className='postHeader'>
-      <img className='avatar' src="" alt="" />
-      <div className='infoPost'>
+      <img className='avatar' src="https://avatarfiles.alphacoders.com/170/thumb-170221.png" alt="" />
+      <div className='details'>
         <span>Júlio Alcantara</span>
         <span>04 Jun 2019</span>
       </div>
@@ -18,9 +18,9 @@ function PostComments() {
     <div className='postComment'>
       <div className="divider" />
         <div className="comment">
-          FOTO
+          <img className='avatar' src="https://avatarfiles.alphacoders.com/170/thumb-170221.png" />
           <p>
-            <span>Diego Fernandes</span>
+            <span>Diego Fernandes:</span>
             A Rocketseat está sempre em busca de novos membros para o time, e geralmente 
             ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de 
             devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar 
@@ -34,9 +34,9 @@ function PostComments() {
 
 function PostItem() {
   return (
-  <div className='postItem'>
+  <div className='post'>
     <PostHeader />
-    <p>Pessoal, alguém sabe se a Rocketseat está contratando?</p>
+    <p className='postContent'>Pessoal, alguém sabe se a Rocketseat está contratando?</p>
     <PostComments />
   </div>
   )
